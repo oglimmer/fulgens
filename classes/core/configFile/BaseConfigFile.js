9f8440f228cd953e7d3d9ecc6c3a27325a5f5524
Oli Zimpasser <oglimmer@gmail.com>
2018-12-27 18:33:04 +0100

    Support http/https for LoadDefaultContent

--- classes/core/configFile/BaseConfigFile.js
+++ classes/core/configFile/BaseConfigFile.js
@@ -6,6 +6,27 @@ const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
 
+const getContent = function(url) {
+  // return new pending promise
+  return new Promise((resolve, reject) => {
+    // select http or https module, depending on reqested url
+    const lib = url.startsWith('https') ? require('https') : require('http');
+    const request = lib.get(url, (response) => {
+      // handle http errors
+      if (response.statusCode < 200 || response.statusCode > 299) {
+         reject(new Error('Failed to load page, status code: ' + response.statusCode));
+       }
+      // temporary data holder
+      const body = [];
+      // on every content chunk, push it to the data array
+      response.on('data', (chunk) => body.push(chunk));
+      // we are done, resolve promise with those joined chunks
+      response.on('end', () => resolve(body.join('')));
+    });
+    // handle connection errors of the request
+    request.on('error', (err) => reject(err))
+    })
+};
 
 class BaseConfigFile {
 
@@ -24,17 +45,26 @@ class BaseConfigFile {
     this.TmpFolder = hash.digest('hex').substring(0, 8);
   }
 
-  createFile() {
+  async createFile() {
     // start with loading default content if given
     var content = [];
     if (this.LoadDefaultContent) {
-      const filename = path.resolve(this.runtimeConfiguration.projectBasePath, this.LoadDefaultContent.replace('$$TMP$$', 'localrun'));
-      if (!fs.existsSync(filename)) {
-        throw `${filename} not found`;
+      var contentBlock;
+      if (this.LoadDefaultContent.startsWith('http')) {
+        try {
+          contentBlock = await getContent(this.LoadDefaultContent);
+        } catch (err) {
+          throw `Failed to load URL ${this.LoadDefaultContent}`;
+        }
+      } else {
+        const filename = path.resolve(this.runtimeConfiguration.projectBasePath, this.LoadDefaultContent.replace('$$TMP$$', 'localrun'));
+        if (!fs.existsSync(filename)) {
+          throw `${filename} not found`;
+        }
+        contentBlock = fs.readFileSync(filename, { encoding: 'utf8' });
       }
-      const contentBlock = fs.readFileSync(filename, { encoding: 'utf8' });
       content = contentBlock.split(/\r?\n/);
-      content = content.map(l => l.replace('$', '\\$'));
+      content = content.map(l => l.replace('$', '\\$'));        
     }
     // replace all variables given via this.Content (Regexp, Line)
     this.Content.forEach(contentLine => {
